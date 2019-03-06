bfa92cf4931d8b25c4d8a94e3b7d3867f7f229c6
vijeta rathod <vijeta.rathod@zymr.com>
2019-03-06 15:01:56 +0530

    textbox drag n drop functionality

--- src/app/components/dragable/dragable.component.ts
+++ src/app/components/dragable/dragable.component.ts
@@ -13,8 +13,12 @@ export class DragableComponent implements OnInit {
     {name: 'Label', id: 2},
     {name: 'Header', id: 3},
     {name: 'Link', id: 4}];
+    public size: number;
+    public elementId: string;
+    public elementValue: string;
+    public elementCount = 1;
 
-  constructor(private renderer: Renderer2) { }
+  constructor(el: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
   }
@@ -27,10 +31,21 @@ export class DragableComponent implements OnInit {
     console.log('in element drop metthod', e.dragData);
     if (e.dragData.id === 1) {
       const input = this.renderer.createElement('input');
+      this.renderer.setProperty(input, 'id', 'txt' + this.elementCount);
       this.renderer.appendChild(this.div.nativeElement, input);
+      this.renderer.listen(input, 'click', (event) => {
+        console.log('onclick event', event);
+        const target = event.target || event.srcElement || event.currentTarget;
+        const Attr = target.attributes;
+        this.elementId = Attr[0].ownerElement.id;
+        this.size = Attr[0].ownerElement.size;
+        this.elementValue = Attr[0].ownerElement.value;
+      });
+      this.elementCount ++;
+      // input.addEventListener('click', this.onClick.bind(this));
       // this.div.nativeElement.insertAdjacentHTML('beforeend', '<input type="text" #text1 onclick="showProperty(1)">');
     } else if (e.dragData.id === 2) {
-      this.div.nativeElement.insertAdjacentHTML('beforeend', '<label for="lbl" onclick="showProperty(2);">Label</label>');
+      this.div.nativeElement.insertAdjacentHTML('beforeend', '<label for="lbl">Label</label>');
     } else if (e.dragData.id === 3) {
       this.div.nativeElement.insertAdjacentHTML('beforeend', '<h1>Header</h1>');
     } else if (e.dragData.id === 4) {
@@ -39,8 +54,12 @@ export class DragableComponent implements OnInit {
       this.div.nativeElement.insertAdjacentHTML('beforeend', '<div></div>');
     }
   }
-
-  showProperty(element: number) {
-    console.log('element id', element);
+  onClick(event) {
+    console.log('onclick event', event);
+  }
+  onPropertyChange(event: any, property: string) {
+    console.log('on property change', event.target.value);
+    const elem: Element = document.getElementById(this.elementId);
+    this.renderer.setAttribute(elem, property, event.target.value);
   }
 }
